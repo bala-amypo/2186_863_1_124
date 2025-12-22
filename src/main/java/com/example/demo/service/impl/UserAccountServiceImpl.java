cb89b2845cb1198f992bc3b5b0dc199780339857
bala-amypo <bala@amypo.in>
2025-12-22 00:35:13 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserAccountServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserAccountServiceImpl.java
@@ -5,26 +5,23 @@ import com.example.demo.exception.BadRequestException;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.UserAccountRepository;
 import com.example.demo.service.UserAccountService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserAccountServiceImpl implements UserAccountService {
 
     private final UserAccountRepository repository;
-    private final PasswordEncoder passwordEncoder;
 
-    public UserAccountServiceImpl(UserAccountRepository repository,
-                                  PasswordEncoder passwordEncoder) {
+
+    public UserAccountServiceImpl(UserAccountRepository repository) {
         this.repository = repository;
-        this.passwordEncoder = passwordEncoder;
+      
     }
 
     public UserAccount register(UserAccount user) {
         if (repository.existsByEmail(user.getEmail()))
             throw new BadRequestException("Email already exists");
 
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return repository.save(user);
     }
 
