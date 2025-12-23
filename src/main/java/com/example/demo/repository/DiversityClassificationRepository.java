1a79e8a508df1ac849a7418fe12b69d3808d13b5
bala-amypo <bala@amypo.in>
2025-12-23 17:29:20 +0000

    User commit

--- src/main/java/com/example/demo/repository/DiversityClassificationRepository.java
+++ src/main/java/com/example/demo/repository/DiversityClassificationRepository.java
@@ -2,10 +2,11 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.DiversityClassification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
-@Repository
-public interface DiversityClassificationRepository extends JpaRepository<DiversityClassification, Long> {
+public interface DiversityClassificationRepository
+        extends JpaRepository<DiversityClassification, Long> {
+
     List<DiversityClassification> findByActiveTrue();
 }
--- src/main/java/com/example/demo/repository/DiversityTargetRepository.java
+++ src/main/java/com/example/demo/repository/DiversityTargetRepository.java
@@ -2,11 +2,13 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.DiversityTarget;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
-@Repository
-public interface DiversityTargetRepository extends JpaRepository<DiversityTarget, Long> {
+public interface DiversityTargetRepository
+        extends JpaRepository<DiversityTarget, Long> {
+
     List<DiversityTarget> findByTargetYear(int targetYear);
+
     List<DiversityTarget> findAll();
 }
