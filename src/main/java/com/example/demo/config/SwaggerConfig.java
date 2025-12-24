38c319190f0d1daa2e26b536bd017f9a4e4276d0
bala-amypo <bala@amypo.in>
2025-12-24 04:24:29 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,20 +1,26 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 @Configuration
-public class OpenApiConfig {
+public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                // You need to change the port as per your server
+                .info(new Info()
+                        .title("Supplier Diversity Tracker")
+                        .version("1.0")
+                        .description("API documentation for AmyPO test cases")
+                )
                 .servers(List.of(
                         new Server().url("https://9077.32procr.amypo.ai/")
                 ));
-        }
+    }
 }
