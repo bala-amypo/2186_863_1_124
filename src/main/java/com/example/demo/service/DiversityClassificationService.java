0f2fc921a6d13b483915bf15d70597a2c2715aa0
bala-amypo <bala@amypo.in>
2025-12-26 04:24:09 +0000

    User commit

--- src/main/java/com/example/demo/service/DiversityClassificationService.java
+++ src/main/java/com/example/demo/service/DiversityClassificationService.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 public interface DiversityClassificationService {
     DiversityClassification createClassification(DiversityClassification classification);
+    DiversityClassification updateClassification(Long id, DiversityClassification classification);
+    DiversityClassification getClassificationById(Long id);
+    List<DiversityClassification> getAllClassifications();
     List<DiversityClassification> getActiveClassifications();
     DiversityClassification deactivateClassification(Long id);
 }
