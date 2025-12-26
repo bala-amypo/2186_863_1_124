d26e4c9a42d6fb6cb25c9ae2d5663b6262aae1ba
bala-amypo <bala@amypo.in>
2025-12-26 08:22:52 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -1,10 +1,11 @@
 package com.example.demo.servlet;
-
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+
 
 @WebServlet(urlPatterns = "/simple-status")
 
